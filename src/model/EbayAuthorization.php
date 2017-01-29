ca1584cc81a2b6d5cb9b51da7b08a6943ac7343f
James McGing <jamesmcging@gmail.com>
2017-01-29 15:21:59 +0000

    [add] Authorization CURL request

--- src/model/EbayAuthorization.php
+++ src/model/EbayAuthorization.php
@@ -9,7 +9,8 @@ class EbayAuthorization {
   private $_sClientSecret    = '';
   private $_sRuName          = '';
   private $_sEbayAccessToken = '';
-  private $_sEbayOAuthURL    = '';
+  private $_sEbayOAuthURL    = 'https://api.sandbox.ebay.com/identity/v1/oauth2/token';
+  
   
   public function __construct() {
     if (self::PRODUCTION) {
@@ -25,6 +26,29 @@ class EbayAuthorization {
   
   public function requestAccessToken() {
     
+    $rscConnection = curl_init();
+    curl_setopt($rscConnection, CURLOPT_URL, $this->_sEbayOAuthURL);
+    curl_setopt($rscConnection, CURLOPT_POST);
+    
+    $arrHeaders = array();
+    $arrHeaders[] = 'Content-type: application/x-www-form-urlencoded';
+    $arrHeaders[] = 'Authorization: Basic '.base64_encode($this->_sClientId.':'.$this->_sClientSecret);
+    
+    curl_setopt($rscConnection, CURLOPT_HTTPHEADER, $arrHeaders);
+    
+    curl_setopt($rscConnection, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($rscConnection, CURLOPT_SSL_VERIFYHOST, 0);
+    
+    curl_setopt($rscConnection, CURLOPT_RETURNTRANSFER, 1);
+    
+    $sBody = "grant_type=client_credentials&redirect_uri={$this->_sRuName}";
+    curl_setopt($rscConnection, CURLOPT_POSTFIELDS, $sBody);
+    
+    $sResponse = curl_exec($rscConnection);
+    
+    curl_close($rscConnection);
+    
+    var_dump($sResponse);
   }
   
   public function getAccessToken() {
